0dd5dfac8ec66978f9e6e3c01d0498edb57c9d5b
Ignacio <1992ignacio@gmail.com>
2024-08-08 11:13:54 -0700

    Initial Commit

---
+++ QuickScanner/QuickScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickScannerApp.swift
+//  QuickScanner
+//
+//  Created by Ignacio Arias on 2024-08-08.
+//
+
+import SwiftUI
+
+@main
+struct QuickScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
